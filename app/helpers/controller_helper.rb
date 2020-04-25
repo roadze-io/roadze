fcbc66538f2af26a81cc0f64d5534c53bb3327f7
TheMindlessDev <themindlessdev@protonmail.com>
2020-04-25 00:57:52 -0700

    Completes md, lg & xl screen size layout

---
+++ app/helpers/controller_helper.rb
@@ -0,0 +1,13 @@
+module ControllerHelper
+  def current_request?(*requests)
+    requests.each do |request|
+      next unless request[:controller] == controller.controller_name
+      if request[:action].is_a?(Array) && request[:action].include?(controller.action_name)
+        return true
+ 			end
+      return true if request[:action] == controller.action_name
+    end
+    false
+	end
+	# current_request?(controller: 'users', action: ['new', 'create'])
+end
