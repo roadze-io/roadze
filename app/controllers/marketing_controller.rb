2f55637db772ce228580f6f0758e81cf4c05dbe6
TheMindlessDev <themindlessdev@protonmail.com>
2020-04-25 18:19:08 -0700

    Adds ahoy gem to marketing

--- app/controllers/marketing_controller.rb
+++ app/controllers/marketing_controller.rb
@@ -3,23 +3,31 @@ class MarketingController < ApplicationController
     @page_title       = 'Home'
     @page_description = 'roadze.io landing page'
     @page_keywords    = 'Roadze, Site, Homepage, Loadboard, Canada loadboard, Logistics, Trucking'
+
+    ahoy.track "Viewed the page", title: @page_title
   end
 
   def features
     @page_title       = 'Features'
     @page_description = 'roadze.io features page'
     @page_keywords    = 'Roadze, Site, Features, Loadboard, Canada loadboard, Logistics, Trucking'
+
+    ahoy.track "Viewed the page", title: @page_title
   end
 
   def contact
     @page_title       = 'Contact us'
     @page_description = 'roadze.io contact us page'
     @page_keywords    = 'Roadze, Site, Contact us, Loadboard, Canada loadboard, Logistics, Trucking'
+
+    ahoy.track "Viewed the page", title: @page_title
   end
 
   def pricing
     @page_title       = 'Pricing'
     @page_description = 'roadze.io product pricing page'
     @page_keywords    = 'Roadze, Site, Pricing, Loadboard, Canada loadboard, Logistics, Trucking'
+
+    ahoy.track "Viewed the page", title: @page_title
   end
 end
